0e9e50a1419ab44f74b9c48d43ea805b4fc8e51c
ana_mendes <ana@seox.com.br>
2022-08-07 10:02:34 -0300

    Configurando arquivos relacionados aos repositorios dos cruds

--- src/main/java/dcc193/trabalho/avaliacao/ObservacaoRepository.java
+++ src/main/java/dcc193/trabalho/avaliacao/ObservacaoRepository.java
@@ -1,5 +1,7 @@
 package dcc193.trabalho.avaliacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ObservacaoRepository extends JpaRepository<Observacao, Long> {
 
-public class ObservacaoRepository {
-    
 }
--- src/main/java/dcc193/trabalho/avaliacao/ProjetoRepository.java
+++ src/main/java/dcc193/trabalho/avaliacao/ProjetoRepository.java
@@ -1,6 +1,8 @@
 package dcc193.trabalho.avaliacao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
 
-public class ProjetoRepository {
-    
 }
