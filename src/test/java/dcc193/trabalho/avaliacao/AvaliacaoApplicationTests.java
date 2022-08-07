f4e0c128ace4ee72bc49603555fe99fe65b8d598
ana_mendes <ana@seox.com.br>
2022-08-07 09:13:54 -0300

    Criação dos arquivos base

---
+++ src/test/java/dcc193/trabalho/avaliacao/AvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package dcc193.trabalho.avaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
